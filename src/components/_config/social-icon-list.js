01e18b34dd549dcda2f1cff9ed56631ff49cdfe6
Charlotte <ckdrew670@hotmail.com>
2020-12-10 16:16:55 +0000

    Add soundcloud social icon

--- src/components/_config/social-icon-list.js
+++ src/components/_config/social-icon-list.js
@@ -3,6 +3,11 @@ export const socialIconList = [
     name: 'instagram',
     prefix: 'fab', // fab prefix for brands, fas for solid icons
     link: 'https://www.instagram.com/pete_plays_sax/',
+  },
+  {
+    name: 'soundcloud',
+    prefix: 'fab', // fab prefix for brands, fas for solid icons
+    link: 'https://soundcloud.com/glassparc',
   }
   // Add further social links with the icon of choice and link here
   // check https://fontawesome.com/icons?d=gallery&s=brands for other brand icons
